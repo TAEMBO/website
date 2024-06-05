1f6f51f0fe3c85b27a95b19733f15cd0430c55fe
TÆMBØ <kuneshr123@gmail.com>
2024-06-05 16:28:53 -0700

    feat: Update content

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import Logo from "./components/Logo";
 import Hobbies from "./components/Hobbies";
+import Heading from "./components/Heading";
 
 export default function App() {
     return (
         <>
-            <header className="block">sus</header>
-            <h2 className="block">
-                We do quite a considerable amount of mental trickery and mockery of those who are unfortunate enough to fall victim to our clever little trap of social teasing
-            </h2>
+            <Heading />
             <br />
-            <h3 className="block">
-                <s>Sorry dark mode users, coming soon&trade;</s> dark mode woooo (but now no light mode)
-            </h3>
+            <br />
             <Hobbies />
             <br />
             <Logo />
---
+++ src/components/Heading.tsx
@@ -0,0 +1,7 @@
+export default function Heading() {
+    return (
+        <h2 className="block">
+            Your average Silicon Valley hardware & software tech enthusiast
+        </h2>
+    );
+}
